f045b58949d94e4e9aeeb60ddfcafe0719744671
Franciszek Czajkowski <FCzajkowski@proton.me>
2026-09-18 18:49:52 +0200

    utworzenie klasy Electronics

--- kod.java
+++ kod.java
@@ -19,9 +19,35 @@ class PRODUCT
     }
 }
 
+class Electronics extends PRODUCT
+{
+    private String manufacturer;
+    private int warrantyMonths;
+
+    public Electronics(String name, double basePrice, String manufacturer, int warrantyMonths) {
+        super(name, basePrice);
+        this.manufacturer = manufacturer;
+        this.warrantyMonths = warrantyMonths;
+    };
+
+    @Override
+    public String displayDetails()
+    {
+        super.displayDetails();
+        System.out.println("Manufacturer: " + manufacturer);
+        System.out.println("Warranty: " + warrantyMonths + " months");
+        return name + ", " + basePrice + ", " + manufacturer + ", " + warrantyMonths;
+    }
+}
+
+
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Product> inventory = new ArrayList<>();
 
+        Electronics telephone = new Electronics("telephone", 4200.0, "ijapuszko", 18);
+
+        inventory.add(telephone);
     }
 }
